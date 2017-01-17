4a75262a30ed3e812f132117c77819613309ab3d
Russell Richie <russell.richie@uconn.edu>
2017-01-16 21:40:45 -0500

    simplify code for class

--- COGSTools/cohortModel.py
+++ COGSTools/cohortModel.py
@@ -24,8 +24,8 @@ def cohortModel(word, EnglishWords):
         for word in list(candidates):          # now look through the candidate words
             if not word.startswith(soFar):     # if the word seen so far is NOT consistent with a word we know
                 candidates.remove(word)        # remove the word from the candidates
-        print("These are the possible words when we've heard '{}' so far:\n{}".format(soFar,candidates))
-    print("After hearing everything, the only possible word(s) are {}".format(candidates))
+        print("When we've heard '{}' so far, the canddiates are:\n{}".format(soFar,candidates))
+    print("After hearing everything, the only possible word(s): {}".format(candidates))
     return candidates
 
 # Test the cohort model with a fragment of English
